78c3c58da36939401b78abe010decdadf5a7e549
Sarah_KCAP <sarah.khader@capillarytech.com>
2024-02-26 15:53:48 +0530

    Assignment 1

--- src/App.test.js
+++ src/App.test.js
@@ -3,25 +3,43 @@ import App from "./App";
 
 test("Test counter App", () => {
   render(<App />);
-  /*
-  TODO:
-    assert if initial count value is 0 on render
-    assert if increment button is present
-    assert if decrement button is present
-    click increment button twice
-    assert if count value is 2
-    click decrement button once
-    assert if count value is 1
-    click decrement button once
-    assert if count value is 0
-    click decrement button once
-    assert if count value is 0 if below 0 it wont decrement
+  // assert if initial count value is 0 on render
+  const counterText=screen.getByTestId("counter-test")
+  expect(counterText).toHaveTextContent(0)
 
-    hints:
-    for getting dom elements use getBy/queryBy from screen
-    use getByRole/findByRole query to find 'increment' button
-    use fireevent or user event to click
-    make assertions using expect with screen.getBy or queryBy
-    repeat the above steps dependent on test case requirement in todo
-  */
+  // assert if increment button is present
+  const incrementButton=screen.getByRole("button",{name:"Increment"})
+  expect(incrementButton).toBeInTheDocument()
+
+
+  //assert if decrement button is present
+  const decrementButton=screen.getByRole("button",{name:"Decrement"})
+  expect(decrementButton).toBeInTheDocument()
+  
+  //click increment button twice
+  fireEvent.click(incrementButton)
+  fireEvent.click(incrementButton)
+
+  //assert if count value is 2
+  expect(counterText).toHaveTextContent(2)
+
+  //click decrement button once
+  fireEvent.click(decrementButton)
+
+  //assert if count value is 1
+  expect(counterText).toHaveTextContent(1)
+
+  //click decrement button once
+  fireEvent.click(decrementButton)
+
+  //assert if count value is 0
+  expect(counterText).toHaveTextContent(0)
+
+  //click decrement button once
+  //assert if count value is 0 if below 0 it wont decrement
+
+  fireEvent.click(decrementButton)
+  expect(counterText).toHaveTextContent(0)
+
+ 
 });
